6abbba6cacbcc3694127dbc7c6578ff1e1a0a9ad
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-11-03 17:09:42 +0500

    imp 2

--- lib/widgets/scroll_example.dart
+++ lib/widgets/scroll_example.dart
@@ -18,14 +18,11 @@ class _ScrollExampleState extends State<ScrollExample> {
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  Container(height: 200, color: Colors.red),
-                  Container(height: 200, color: Colors.yellow),
-                  Container(height: 200, color: Colors.pink),
-                  Container(height: 200, color: Colors.black),
-                  Container(height: 200, color: Colors.orange),
-                  Container(height: 200, color: Colors.blue),
-                  Container(height: 200, color: Colors.purple),
-                  Container(height: 200, color: Colors.indigo),
+                  for (int i = 0; i < 10; i++)
+                    Container(
+                      height: 200,
+                      color: i.isEven ? Colors.red : Colors.indigo,
+                    ),
                 ],
               ),
             ),
